f9578282090f6cac89a8fc9462935a327e4a441c
Sarah <sarah.mattar@mac.com>
2018-11-06 18:20:53 -0800

    removed extraneous code in NotFound.js

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-// export default class NotFound extends React.Component {
-//   render() {
-//     return <p>NotFound component here</p>;
-//   }
-// }
-
 export default () => {
 	return (
 		<div className="boxed-view">
